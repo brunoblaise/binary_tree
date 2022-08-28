36d1b44f504bf7dd1b3ec62f29106fa57b81c2eb
Bruno Blaise <brunoblaise94@gmail.com>
2022-08-28 19:33:30 +0200

    python

---
+++ cry.py
@@ -0,0 +1,10 @@
+china = int(input("What do you have left in yuan?"))
+japan = int(input("What do you have left in yen?"))
+korean = int(input("What do you have left in won?"))
+
+
+
+total =(china * 0.15) + (japan * 0.0073) + (korean * 0.00074) 
+
+print("You have $" + str(total) + " left.")
+
